26222105021dfce0aa3e8c9c762d545566d4f808
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-01-01 13:29:29 -0200

    Inline display_temperature and control display with macro

--- Aorist.ino
+++ Aorist.ino
@@ -1,14 +1,18 @@
 #include <LedControl.h>
 #include <DS3231.h>
 
-#define DIN 5
-#define CS  6
-#define CLK 7
+#define DIN A0
+#define CS  A1
+#define CLK A2
 
 #define MAX7219_AMOUNT 1
 
 LedControl lc(DIN,CLK,CS,MAX7219_AMOUNT);
 DS3231     rtc(SDA, SCL);
+
+#define DISPLAY_2DIG(N,POS,DOT) \
+  lc.setDigit(0, POS,   N / 10, false); \
+  lc.setDigit(0, POS-1, N % 10, DOT);
 
 void setup()
 {
@@ -16,22 +20,19 @@ void setup()
   lc.clearDisplay(0);
   lc.setIntensity(0,1);
   rtc.begin();
-  // The following lines can be uncommented to set the date and time
+  display_temperature();
+  // Uncomment the following lines to set date and time
   //rtc.setDOW(FRIDAY);
   //rtc.setTime( 3, 48, 0);
   //rtc.setDate(21,  4, 2017);
-  display_temperature();
 }
 
 void loop()
 {
   Time t = rtc.getTime();
-  lc.setDigit(0,7,t.hour / 10,false);
-  lc.setDigit(0,6,t.hour % 10,true);
-  lc.setDigit(0,5,t.min  / 10,false);
-  lc.setDigit(0,4,t.min  % 10,true);
-  lc.setDigit(0,3,t.sec  / 10,false);
-  lc.setDigit(0,2,t.sec  % 10,true);
+  DISPLAY_2DIG(t.hour, 7, true);
+  DISPLAY_2DIG(t.min,  5, true);
+  DISPLAY_2DIG(t.sec,  3, true);
   if(t.sec == 0)
   {
     display_temperature();
@@ -39,9 +40,8 @@ void loop()
   delay(900);
 }
 
-void display_temperature()
+inline void display_temperature()
 {
   int temp = (int)rtc.getTemp();
-  lc.setDigit(0,1,temp / 10,false);
-  lc.setDigit(0,0,temp % 10,false);
+  DISPLAY_2DIG(temp, 1, false);
 }

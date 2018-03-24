6f537f7de91ec408ced735cc4d93be54c4237ef5
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-03-24 05:51:24 -0300

    Use timer1 at 1Hz instead of delay

--- Aorist.ino
+++ Aorist.ino
@@ -22,9 +22,24 @@ void setup()
   //rtc.setDOW(FRIDAY);
   //rtc.setTime( 3, 48, 0);
   //rtc.setDate(21,  4, 2017);
+  noInterrupts();
+  // Set timer1 interrupt at 1Hz
+  TCCR1A = TCCR1B = 0;
+  TCNT1 = 15623;
+  // Set compare match register for 1Hz increments
+  OCR1A = 15624; // (16_000_000) / 1024 - 1
+  // Turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  // Enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
 }
 
-void loop()
+void loop(){}
+
+ISR(TIMER1_COMPA_vect)
 {
   Time t = rtc.getTime();
   DISPLAY_2DIG(t.hour, 7, true);
@@ -34,7 +49,6 @@ void loop()
   {
     display_temperature();
   }
-  delay(900);
 }
 
 inline void display_temperature()
